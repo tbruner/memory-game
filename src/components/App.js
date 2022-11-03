12e6c93e3a0c4b3139aa54940ea9116e2004349e
tbruner <trevor.a.bruner@gmail.com>
2022-11-02 21:08:30 -0400

    Implement functionality to display birds

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,82 @@
+import { useState } from 'react';
 import '../styles/App.css';
+import Cards from './Cards.js';
+import Score from './Score.js';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [best, setBest] = useState(0);
+  const [cards, setCards] = useState([
+    {
+      name: 'crow',
+      used: false
+    },
+    {
+      name: 'robin',
+      used: false
+    },
+    {
+      name: 'wigeon',
+      used: false
+    },
+    {
+      name: 'blueJay',
+      used: false
+    },
+    {
+      name: 'hawk',
+      used: false
+    },
+    {
+      name: 'wren',
+      used: false
+    },
+    {
+      name: 'woodpecker',
+      used: false
+    },
+    {
+      name: 'cardinal',
+      used: false
+    },
+    {
+      name: 'blackbird',
+      used: false
+    },
+    {
+      name: 'nuthatch',
+      used: false
+    },
+    {
+      name: 'warbler',
+      used: false
+    },
+    {
+      name: 'dove',
+      used: false
+    }
+  ]);
+
+  function updateGame(e) {
+
+  }
+
   return (
     <div className="App">
+      <header>
+        <div className="title-card">
+          <h1 className='title'>Birds Memory Game</h1>
+          <p className='description'>Get points by clicking on an image, but don't click the same image twice!</p>
+        </div>
+        <Score
+          score={score}
+          best={best}
+        />
+      </header>
+      <Cards
+        updateGame={updateGame}
+        cards={cards}
+      />
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,66 @@
 import '../styles/Cards.css';
+import crow from '../images/american-crow.jpg';
+import robin from '../images/american-robin.jpg';
+import wigeon from '../images/american-wigeon.jpg';
+import blueJay from '../images/blue-jay.jpg';
+import hawk from '../images/coopers-hawk.jpg';
+import wren from '../images/carolina-wren.jpg';
+import woodpecker from '../images/hairy-woodpecker.jpg';
+import cardinal from '../images/northern-cardinal.jpg';
+import blackbird from '../images/red-winged-blackbird.jpg';
+import nuthatch from '../images/white-breasted-nuthatch.jpg';
+import warbler from '../images/yellow-rumped-warbler.jpg';
+import dove from '../images/mourning-dove.jpg';
 
-function Cards({ cards }) {
+
+function Cards({ cards, updateGame }) {
+  function checkBird(bird) {
+    if(bird.name == 'crow') {
+      return crow
+    }
+    else if(bird.name == 'blueJay') {
+      return blueJay
+    }
+    else if(bird.name == 'robin') {
+      return robin
+    }
+    else if(bird.name == 'wigeon') {
+      return wigeon
+    }
+    else if(bird.name == 'hawk') {
+      return hawk
+    }
+    else if(bird.name == 'wren') {
+      return wren
+    }
+    else if(bird.name == 'woodpecker') {
+      return woodpecker
+    }
+    else if(bird.name == 'cardinal') {
+      return cardinal
+    }
+    else if(bird.name == 'blackbird') {
+      return blackbird
+    }
+    else if(bird.name == 'nuthatch') {
+      return nuthatch
+    }     
+    else if(bird.name == 'warbler') {
+      return warbler
+    }
+    else if(bird.name == 'dove') {
+      return dove
+    }
+  }
+
   return (
     <div className='cards'>
-      
+      {cards.map((bird, index) => (
+        <div className='card' key={index}>
+          <img src={checkBird(bird)} alt={bird.name} />
+          <p className='card-title'>{bird.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
